6961b8fd90c10efe1618f4755878226833dc710c
Dawood Asghar <dawoodmalhi@outlook.com>
2019-10-07 07:07:21 -0700

    adding more names

--- notepad.cpp
+++ notepad.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-cout<<"Dawood Malhi"<<endl<<"Krona Emmanuel"<<endl<<"Abdullah Saleem"<<endl<<"Ghulam Ghuos"<<"Saad Shafiq"<<"Huzaifa Amjad"<<endl;
+cout<<"Dawood Malhi"<<endl<<"Krona Emmanuel"<<endl<<"Abdullah Saleem"<<endl<<"Ghulam Ghuos"<<"Saad Shafiq"<<"Huzaifa Amjad"<<"Faisal Mehboob"<<endl;
 
 return 0;
 }
